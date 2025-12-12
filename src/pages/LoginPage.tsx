072ac55aa43c09113eae2b2d124eb5f0636a890d
Victoria Ferry <ferryvictoria@hotmail.fr>
2025-12-12 16:59:04 +0100

    add : header, AdminPage, UserPage compos + installation of zustand and creation of the userUseStore to store the response of the api login  + redirection to admin or user pages

---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,13 @@
+import Login from "../components/Login";
+import Header from "../components/Header";
+
+export default function loginPage() {
+    return (
+        <>
+            <Header />
+            <Login />
+        </>
+    )
+
+
+}
---
+++ src/stores/userStore.ts
@@ -0,0 +1,28 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+interface User {
+  id: number;
+  firstname: string;
+  registration_number: string;
+  authorization: string;
+}
+
+interface UserStore {
+  user: User | null;
+  setUser: (user: User) => void;
+  logout: () => void;
+}
+
+export const useUserStore = create<UserStore>()(
+  persist(
+    (set) => ({
+      user: null,
+      setUser: (user: User) => set({ user }),
+      logout: () => set({ user: null }),
+    }),
+    {
+      name: "user-storage", // clé dans localStorage
+    }
+  )
+);
